637e614cfd97ee47223644eb16dbe94565673a71
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2016-07-08 14:50:15 -0400

    new links

--- feed_builder.js
+++ feed_builder.js
@@ -24,26 +24,26 @@ feedBuilder.items = [{
     
 feedBuilder.buildFeed = function (feedOptions, items) {
     var feed = new RSS(feedOptions);
-    items.forEach(function (item) {
-        item.title = chance.name();
-        item.description = chance.paragraph();
-        item.date = new Date().toUTCString();
-        feed.item(item);
-    });
-    feed.item({
-        title: 'hey zeus christo',
-        description: 'some guy',
-        url: 'http://bible2.com',
-        guid: 'http://bible2.com',
-        date: new Date().toUTCString()
-    });
-    feed.item({
-        description: 'description3',
-        url: 'http://google.com/ABC',
-        guid: 'http://google.com/ABC',
-        title: 'This should never change!',
-        date: new Date(staticDate).toUTCString()
-    });
+    //items.forEach(function (item) {
+    //    item.title = chance.name();
+    //    item.description = chance.paragraph();
+    //    item.date = new Date().toUTCString();
+    //    feed.item(item);
+    //});
+    //feed.item({
+    //    title: 'hey zeus christo',
+    //    description: 'some guy',
+    //    url: 'http://bible2.com',
+    //    guid: 'http://bible2.com',
+    //    date: new Date().toUTCString()
+    //});
+    //feed.item({
+    //    description: 'description3',
+    //    url: 'http://google.com/ABC',
+    //    guid: 'http://google.com/ABC',
+    //    title: 'This should never change!',
+    //    date: new Date(staticDate).toUTCString()
+    //});
     //feed.item({
     //    description: 'Kathrina',
     //    url: 'http://www.aol.com',
@@ -51,13 +51,13 @@ feedBuilder.buildFeed = function (feedOptions, items) {
     //    title: 'This is that new new',
     //    date: 'Fri, 08 Jul 2016 15:05:40 GMT' 
     //});
-    //feed.item({
-    //    description: 'This is the newest of the new',
-    //    url: 'http://www.ryanreynolds.com',
-    //    guid: 'http://www.ryanreynolds.com',
-    //    title: 'Ryan Reynolds',
-    //    date: new Date().toUTCString()
-    //});
+    feed.item({
+        description: 'This is the newest of the new',
+        url: 'http://www.ryanreynolds5.com',
+        guid: 'http://www.ryanreynolds5.com',
+        title: 'Ryan Reynolds < Ryan Dwyer',
+        date: new Date().toUTCString()
+    });
     var xml = feed.xml();
     return xml;
 };
