ce2586a89f8a5a5c57cf3b39186e03e76b6e285e
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2016-07-18 11:23:39 -0400

    new links

--- feed_builder.js
+++ feed_builder.js
@@ -6,62 +6,41 @@ var feedBuilder = {};
 var staticDate = 1466534277639;
 
 feedBuilder.feedOptions = {
-    title: 'Bloomberg 30 Minutes',
+    title: 'Bloomberg Prod Test 1',
     description: 'Simple test RSS feed with an updating pubDate field.',
     feed_url: '',
     site_url: ''
 }
 
 feedBuilder.items = [{
-        url: 'http://www.mnvbrsdfg.com',
-        guid: 'http://www.mnvbrsdfg.com',
+        url: 'http://www.adfasdfpoiuiuyoy.com',
+        guid: 'http://www.adfasdfpoiuiuyoy.com',
         author: 'Ryan Dwyer'
     }, {
-        url: 'http://www.poiuqweradfas.com',
-        guid: 'http://www.poiuqweradfas.com',
+        url: 'http://www.poiupoiupoiaudsfasdf.com',
+        guid: 'http://www.poiupoiupoiaudsfasdf.com',
         author: 'Ryan Dwyer'
     }, {
-        url: 'http://www.vfewadswfgwer.com',
-        guid: 'http://www.vfewadswfgwer.com',
+        url: 'http://www.xcvbxvciouyiouy.com',
+        guid: 'http://www.xcvbxvciouyiouy.com',
         author: 'Ryan Dwyer'
     }];
     
 feedBuilder.buildFeed = function (feedOptions, items) {
     var feed = new RSS(feedOptions);
-    //items.forEach(function (item) {
-    //    item.title = chance.name();
-    //    item.description = chance.paragraph();
-    //    item.date = new Date().toUTCString();
-    //    feed.item(item);
-    //});
-    //feed.item({
-    //    title: 'Post 30 minuter',
-    //    description: 'some guy',
-    //    url: 'http://jkhasdfhgqweryug.com',
-    //    guid: 'http://jkhasdfhgqweryug.com',
-    //    date: new Date().toUTCString()
-    //});
-    //feed.item({
-    //    description: 'description3',
-    //    url: 'http://google.com/qwefsadsfdtbv',
-    //    guid: 'http://google.com/qwefsadsfdtbv',
-    //    title: '1234 Testing Testing VERSION 2',
-    //    date: new Date(staticDate).toUTCString()
-    //});
+    items.forEach(function (item) {
+        item.title = chance.name();
+        item.description = chance.paragraph();
+        item.date = new Date().toUTCString();
+        feed.item(item);
+    });
     feed.item({
         description: 'The final',
-        url: 'http://www.qwerqwerasdfasdfasd.com',
-        guid: 'http://www.qwerqwerasdfasdfasd.com',
+        url: 'http://www.vfvdvfyodsufygayugf.com',
+        guid: 'http://www.vfvdvfyodsufygayugf.com',
         title: 'Knock on wood....',
-        date: new Date().toUTCString() 
+        date: new Date(staticDate).toUTCString() 
     });
-    //feed.item({
-    //    description: 'This is the newest of the new',
-    //    url: 'http://www.ryanreynolds7.com',
-    //    guid: 'http://www.ryanreynolds7.com',
-    //    title: 'Who the hell is ryan reynolds',
-    //    date: new Date().toUTCString()
-    //});
     var xml = feed.xml();
     return xml;
 };
