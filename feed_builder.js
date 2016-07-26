f1ae773fd9519288d7aa4e01dfa33053790e96e5
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2016-07-26 14:58:36 -0400

    set up to test duplication

--- feed_builder.js
+++ feed_builder.js
@@ -13,32 +13,38 @@ feedBuilder.feedOptions = {
 }
 
 feedBuilder.items = [{
-        url: 'http://www.asdfasdfasdfasd.com',
-        guid: 'http://www.asdfasdfasdfasd.com',
-        author: 'Ryan Dwyer'
+        url: 'http://www.thisisthefirstlinkofthefeed.com',
+        guid: 'http://www.thisisthefirstlinkofthefeed.com',
+        author: 'Ryan Dwyer',
+        title: 'Title Number One ',
+        date: new Date(staticDate).toUTCString()
     }, {
-        url: 'http://www.sdsfsdfdfsdfsfsdsd.com',
-        guid: 'http://www.sdsfsdfdfsdfsfsdsd.com',
-        author: 'Ryan Dwyer'
+        url: 'http://www.thisisthesecondlinkofthefeed.com',
+        guid: 'http://www.thisisthesecondlinkofthefeed.com',
+        author: 'Ryan Dwyer',
+        title: 'Title Number Two ',
+        date: new Date(staticDate).toUTCString()
     }, {
-        url: 'http://www.ertyertyertyerty.com',
-        guid: 'http://www.ertyertyertyerty.com',
-        author: 'Ryan Dwyer'
+        url: 'http://www.thisisthethirdlinkofthefeed.com',
+        guid: 'http://www.thisisthethirdlinkofthefeed.com',
+        author: 'Ryan Dwyer',
+        title: 'Title Number Three ',
+        date: new Date(staticDate).toUTCString()
     }];
     
 feedBuilder.buildFeed = function (feedOptions, items) {
     var feed = new RSS(feedOptions);
     items.forEach(function (item) {
-        item.title = chance.name();
-        item.description = chance.paragraph();
-        item.date = new Date().toUTCString();
+        //item.title = chance.name();
+        //item.description = chance.paragraph();
+        //item.date = new Date().toUTCString();
         feed.item(item);
     });
     feed.item({
         description: 'The final',
-        url: 'http://www.oiuyourityweruiyuioe.com',
-        guid: 'http://www.oiuyourityweruiyuioe.com',
-        title: 'Will not edit....',
+        url: 'http://www.bbbasaaassssswwwww.com',
+        guid: 'http://www.bbbasaaassssswwwww.com',
+        title: 'Title Number Four ',
         date: new Date(staticDate).toUTCString() 
     });
     var xml = feed.xml();
