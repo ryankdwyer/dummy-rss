93cf232def523f84a83722b24dded47048982861
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2016-07-27 09:34:07 -0400

    new links

--- feed_builder.js
+++ feed_builder.js
@@ -6,45 +6,42 @@ var feedBuilder = {};
 var staticDate = 1466534277639;
 
 feedBuilder.feedOptions = {
-    title: 'Bloomberg Tracking Tags Test',
+    title: 'Bloomberg Tracking Tags Duplication Test',
     description: 'Simple test RSS feed with an updating pubDate field.',
     feed_url: '',
     site_url: ''
 }
 
 feedBuilder.items = [{
-        url: 'http://www.thisisthefirstlinkofthefeed.com',
-        guid: 'http://www.thisisthefirstlinkofthefeed.com',
+        url: 'http://www.uyqiuwyeriuoyweiuyrquwei.com',
+        guid: 'http://www.uyqiuwyeriuoyweiuyrquwei.com',
         author: 'Ryan Dwyer',
-        title: 'Title Number One ',
-        date: new Date(staticDate).toUTCString()
+        date: new Date().toUTCString()
     }, {
-        url: 'http://www.thisisthesecondlinkofthefeed.com',
-        guid: 'http://www.thisisthesecondlinkofthefeed.com',
+        url: 'http://www.ggfsfgsdfgsdfgsdfg.com',
+        guid: 'http://www.ggfsfgsdfgsdfgsdfg.com',
         author: 'Ryan Dwyer',
-        title: 'Title Number Two ',
-        date: new Date(staticDate).toUTCString()
+        date: new Date().toUTCString()
     }, {
-        url: 'http://www.thisisthethirdlinkofthefeed.com',
-        guid: 'http://www.thisisthethirdlinkofthefeed.com',
+        url: 'http://www.vvfsdvfsdfvsdfvsdfvsfd.com',
+        guid: 'http://www.vvfsdvfsdfvsdfvsdfvsfd.com',
         author: 'Ryan Dwyer',
-        title: 'Title Number Three ',
-        date: new Date(staticDate).toUTCString()
+        date: new Date().toUTCString()
     }];
     
 feedBuilder.buildFeed = function (feedOptions, items) {
     var feed = new RSS(feedOptions);
     items.forEach(function (item) {
-        //item.title = chance.name();
-        //item.description = chance.paragraph();
-        //item.date = new Date().toUTCString();
+        item.title = chance.name();
+        item.description = chance.paragraph();
+        item.date = new Date().toUTCString();
         feed.item(item);
     });
     feed.item({
         description: 'The final',
-        url: 'http://www.bbbasaaassssswwwww.com',
-        guid: 'http://www.bbbasaaassssswwwww.com',
-        title: 'Title Number Four ',
+        url: 'http://www.asdfasdfpoiuqweriouqwe.com',
+        guid: 'http://www.asdfasdfpoiuqweriouqwe.com',
+        title: 'Title Number Four',
         date: new Date(staticDate).toUTCString() 
     });
     var xml = feed.xml();
