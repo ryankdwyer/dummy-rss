a56ab30783e1c96924ccd32166fc0d1b8cc5eac5
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2016-08-18 09:57:39 -0400

    white space

--- feed_builder.js
+++ feed_builder.js
@@ -6,8 +6,8 @@ var feedBuilder = {};
 var staticDate = 1466534277639;
 
 feedBuilder.feedOptions = {
-    title: 'Bloomberg Tracking Tags Duplication Test',
-    description: 'Simple test RSS feed with an updating pubDate field.',
+    title: 'White space for new client - Liftable',
+    description: 'Test for keeping whtie space',
     feed_url: '',
     site_url: ''
 }
@@ -29,17 +29,10 @@ feedBuilder.items = [{
 feedBuilder.buildFeed = function (feedOptions, items) {
     var feed = new RSS(feedOptions);
     items.forEach(function (item) {
-        item.title = chance.name();
-        item.description = chance.paragraph();
+        item.title = chance.name() + "\n\n" + chance.name();
+        item.description = chance.paragraph() + "\n\n" + chance.paragraph();
         item.date = new Date().toUTCString();
         feed.item(item);
-    });
-    feed.item({
-        description: 'The final',
-        url: 'http://www.nnnbsfgbdfgbdfgbdfgert.com',
-        guid: 'http://www.nnnbsfgbdfgbdfgbdfgert.com',
-        title: 'THIS ONE SHOULD STAY THE SAME',
-        date: new Date(staticDate).toUTCString() 
     });
     var xml = feed.xml();
     return xml;
