d2c37c1f48af9941ccb3ec01c661493048d97ebf
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2016-08-31 09:13:32 -0400

    new links

--- feed_builder.js
+++ feed_builder.js
@@ -13,24 +13,24 @@ feedBuilder.feedOptions = {
 }
 
 feedBuilder.items = [{
-        url: 'http://www.pioiuyerioutweioursjkdhl.com',
-        guid: 'http://www.pioiuyerioutweioursjkdhl.com',
+        url: 'http://www.vsfdvsdfvsdfvsdf.com',
+        guid: 'http://www.vsfdvsdfvsdfvsdf.com',
         author: 'Ryan Dwyer',
     }, {
-        url: 'http://www.vfdsvdfvsdfliuiuyiuo.com',
-        guid: 'http://www.vfdsvdfvsdfliuiuyiuo.com',
+        url: 'http://www.ioiouqerpoiuqweroiuqwoe.com',
+        guid: 'http://www.ioiouqerpoiuqweroiuqwoe.com',
         author: 'Ryan Dwyer',
     }, {
-        url: 'http://www.kkjjkadkfgjasdfgkjdsfgkjadfgkj.com',
-        guid: 'http://www.kkjjkadkfgjasdfgkjdsfgkjadfgkj.com',
+        url: 'http://www.vvfsdfvsdfvsdfvsdfvsdf.com',
+        guid: 'http://www.vvfsdfvsdfvsdfvsdfvsdf.com',
         author: 'Ryan Dwyer',
     }];
     
 feedBuilder.buildFeed = function (feedOptions, items) {
     var feed = new RSS(feedOptions);
     items.forEach(function (item) {
-        item.title = chance.name() + "<br><br>" + chance.name();
-        item.description = chance.paragraph() + "<br><br>" + chance.paragraph();
+        item.title = chance.name() + "<br/><br/>" + chance.name();
+        item.description = chance.paragraph() + "<br/><br/>" + chance.paragraph();
         item.date = new Date().toUTCString();
         feed.item(item);
     });
