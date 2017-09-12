d768c7df7f86e64af84b4168e9125fcc8bb8abfe
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2017-09-12 09:30:29 -0400

    serve static gifs

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var feedBuilder = require('./feed_builder');
 var port = process.env.PORT || 8080;
 
+
+// FOR RSS FEEDS
 //var xml = feedBuilder.buildFeed(feedBuilder.feedOptions, feedBuilder.items);
 //
 //http.createServer(function(req, res) {
@@ -14,16 +18,33 @@ var port = process.env.PORT || 8080;
 //}).listen(port);
 //
 
-fs.readFile('./index.html', function(err, html) {
-    if (err) {
-        throw err;
-    };
 
-    http.createServer(function(req, res) {
-        res.writeHeader(200, {'Content-Type': 'text/html'});
-        res.write(html);
-        res.end();
-    }).listen(port);
-})
+// FOR SERVING INDEX.HTML
+//fs.readFile('./index.html', function(err, html) {
+//    if (err) {
+//        throw err;
+//    };
+//
+//    http.createServer(function(req, res) {
+//        res.writeHeader(200, {'Content-Type': 'text/html'});
+//        res.write(html);
+//        res.end();
+//    }).listen(port);
+//})
+
+// FOR SERVING IMAGES
+http.createServer(function(req, res) {
+    var request = url.parse(req.url, true);
+    var action = request.pathname;
+
+    if (action === '/trump.gif') {
+        var img = fs.readFileSync('./trump.gif');
+        res.writeHead(200, {'Content-Type':'image/gif'});
+        res.end(img, 'binary');
+    } else { 
+     	res.writeHead(200, {'Content-Type': 'text/plain' });
+     	res.end('Hello World \n');
+	}
+}).listen(port);
 
 console.log('Server running at rdwyer.home.saturn.sfsrv.net:' + port);
